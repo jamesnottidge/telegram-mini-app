b7b37e2a2a0279e6d50495bbe6abc80d5ca3e511
jamesnottidge <freshice57@gmail.com>
2025-03-15 16:20:20 +0100

    add make payemnt page component file

---
+++ src/app/make-payment/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import WebApp from "@twa-dev/sdk";
+import { useEffect } from "react";
+import { TonConnectButton } from "@tonconnect/ui-react";
+import RampPaymentInterface from "@/components/PaymentComponent";
+import { ConnectWallet } from "@/components/connect-wallet";
+
+export default function App() {
+  useEffect(() => {
+    WebApp.ready();
+  }, []);
+
+  return (
+    <div className="h-full px-6">
+      {/* <div className="flex justify-end">
+        <TonConnectButton />
+      </div>
+      <div>
+        <RampPaymentInterface />
+      </div> */}
+      <ConnectWallet />
+    </div>
+  );
+}
